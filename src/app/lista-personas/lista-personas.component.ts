0db9b325eb1671a97840f0fe43276a34667e15c6
cecilioAlvarez <cmac@arquitecturajava.com>
2025-02-20 09:23:59 +0100

    nuevos

--- src/app/lista-personas/lista-personas.component.ts
+++ src/app/lista-personas/lista-personas.component.ts
@@ -2,21 +2,28 @@ import { Component } from '@angular/core';
 import { Persona } from '../persona';
 import { NgFor } from '@angular/common';
 import { PersonaRepositoryService } from '../persona-repository.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-personas',
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './lista-personas.component.html',
   styleUrl: './lista-personas.component.css'
 })
 export class ListaPersonasComponent {
 
-  listaPersonas:Persona[]=[];
-  
-  constructor(private personaRepository:PersonaRepositoryService) {
+  listaPersonas: Persona[] = [];
+  personaInsertar: Persona = {} as Persona;
+
+  constructor(private personaRepository: PersonaRepositoryService) {
 
 
-    this.listaPersonas=personaRepository.buscarTodos();
-    }
+    this.listaPersonas = personaRepository.buscarTodos();
+  }
+
+  insertar() {
+    //objeto personainsertar 
+    this.personaRepository.insertar({ ...this.personaInsertar });
+  }
 
 }
